6f3434d52c273e0564fc145968ed51b54a6c1d53
abnerAmos <abner.fsouza1@gmail.com>
2024-12-16 11:42:09 -0300

    - Captura de erro de Token. - Ajuste no timezone local -3h.

--- src/main/java/com/example/login/dto/response/HttpErrorResponse.java
+++ src/main/java/com/example/login/dto/response/HttpErrorResponse.java
@@ -3,15 +3,21 @@ package com.example.login.dto.response;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.Instant;
+import java.time.ZonedDateTime;
 
 public record HttpErrorResponse(
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "America/Sao_Paulo")
     Instant timestamp,
     String status,
     String messageError,
     String path) {
 
     public HttpErrorResponse(String status, String messageError, String path) {
-        this(Instant.now(), status, messageError, path);
+        this(
+                ZonedDateTime.now().toInstant(),
+                status,
+                messageError,
+                path
+        );
     }
 }
--- src/main/java/com/example/login/security/SecurityFilter.java
+++ src/main/java/com/example/login/security/SecurityFilter.java
@@ -3,7 +3,6 @@ package com.example.login.security;
 import com.example.login.exception.ForbiddenException;
 import com.example.login.repository.UserRepository;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
@@ -11,8 +10,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 /**
  * Filtro de segurança responsável por interceptar requisições,
@@ -23,8 +21,8 @@ import java.io.IOException;
 public class SecurityFilter extends OncePerRequestFilter {
 
     private final TokenService tokenService;
-
     private final UserRepository userRepository;
+    private final HandlerExceptionResolver handlerExceptionResolver;
 
     /**
      * Intercepta a requisição HTTP, valida o token JWT e autentica o usuário.
@@ -32,18 +30,23 @@ public class SecurityFilter extends OncePerRequestFilter {
      * @param request  A requisição HTTP recebida.
      * @param response A resposta HTTP que será enviada.
      * @param filterChain A cadeia de filtros para continuar o processamento da requisição.
-     * @throws ServletException Se ocorrer um erro durante o processamento do filtro.
-     * @throws IOException Se ocorrer um erro de entrada/saída.
      */
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var token = tokenRecover(request);
-        var subject = tokenService.getSubject(token);   // Valida o token e extrai o subject (e-mail do usuário)
-        var user =  userRepository.findByEmail(subject);
-        var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities()); // Cria uma instância de autenticação para o usuário
-        SecurityContextHolder.getContext().setAuthentication(authentication); // Configura o contexto de segurança do Spring com os detalhes do usuário autenticado
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
+        try {
+            var token = tokenRecover(request);
 
-        filterChain.doFilter(request, response); // Passa a requisição para o próximo filtro na cadeia
+            if (!token.isEmpty()) {
+                var subject = tokenService.getSubject(token);   // Valida o token e extrai o subject (e-mail do usuário)
+                var user =  userRepository.findByEmail(subject);
+                var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities()); // Cria uma instância de autenticação para o usuário
+                SecurityContextHolder.getContext().setAuthentication(authentication); // Configura o contexto de segurança do Spring com os detalhes do usuário autenticado
+            }
+
+            filterChain.doFilter(request, response); // Passa a requisição para o próximo filtro na cadeia
+        } catch (Exception e) {
+            handlerExceptionResolver.resolveException(request, response, null, e); // Delegar a exceção ao HandlerExceptionResolver
+        }
     }
 
     /**
@@ -57,7 +60,7 @@ public class SecurityFilter extends OncePerRequestFilter {
         if (authorizationHeader == null || authorizationHeader.isEmpty()) {
             throw new ForbiddenException("Token não encontrado");
         } else {
-            return authorizationHeader.replace("Bearer ", "");
+            return authorizationHeader.replace("Bearer ", "").trim();
         }
 
     }
