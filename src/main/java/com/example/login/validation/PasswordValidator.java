e2de6f541ce6cebafe95c0520fcafe86feccb6fb
abnerAmos <abner.fsouza1@gmail.com>
2024-12-19 11:20:19 -0300

    - Adicionado pattern para senhas sequenciais

--- src/main/java/com/example/login/validation/PasswordValidator.java
+++ src/main/java/com/example/login/validation/PasswordValidator.java
@@ -5,18 +5,39 @@ import jakarta.validation.ConstraintValidatorContext;
 
 public class PasswordValidator implements ConstraintValidator<ValidPassword, String> {
 
-    private static final long MAX_LENGTH = 20;
-    private static final long MIN_LENGTH = 8;
     private static final String PASSWORD_PATTERN =
-            "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$";
+            "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&.])[0-9a-zA-Z@$!%*?&.]{8,}$";
+
+    private static final String SEQUENCES_PATTERN =
+            ".*(012|123|234|345|456|567|678|789|890|[a-z]{3}|[A-Z]{3}|([a-zA-Z])\1\1).*";
 
     @Override
     public boolean isValid(String password, ConstraintValidatorContext context) {
         if (password == null) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate("Senha não pode ser nula")
+                    .addConstraintViolation();
+
             return false;
         }
-        return password.matches(PASSWORD_PATTERN)
-                && password.length() >= MIN_LENGTH
-                && password.length() <= MAX_LENGTH;
+
+        if (password.matches(SEQUENCES_PATTERN)) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate("A senha não deve conter sequenciais")
+                    .addConstraintViolation();
+
+            return false;
+        }
+
+        if (!password.matches(PASSWORD_PATTERN)) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate("A senha deve conter ao menos uma letra maiúscula, uma letra minúscula, um número, um caractere especial, e no mínimo 8 caracteres")
+                    .addConstraintViolation();
+
+            return false;
+        }
+
+        return true;
     }
+
 }
