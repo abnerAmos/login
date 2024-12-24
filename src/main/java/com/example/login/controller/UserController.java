ac50dac656df205e47b0e0b0361a38d32b42d4ff
abnerAmos <abner.fsouza1@gmail.com>
2024-12-23 22:15:28 -0300

    - Documentando endpoints

--- src/main/java/com/example/login/controller/UserController.java
+++ src/main/java/com/example/login/controller/UserController.java
@@ -15,6 +15,14 @@ public class UserController {
 
     private final UserService userService;
 
+    /**
+     * Registra um novo usuário na aplicação.
+     *
+     * @param user Contém os dados do usuário a ser registrado.
+     *             A validação é realizada usando as anotações de validação (@Valid) no objeto.
+     * @return Uma resposta HTTP contendo o status 201 (Created) e uma mensagem de sucesso.
+     *         Caso ocorra alguma falha na validação ou processamento, uma exceção será lançada.
+     */
     @PostMapping("/register")
     public ResponseEntity<HttpSucessResponse> register(@RequestBody @Valid User user) {
         var httpResponse = new HttpSucessResponse(HttpStatus.CREATED, "Cadastro efetuado com sucesso");
@@ -23,6 +31,13 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(httpResponse);
     }
 
+    /**
+     * Busca as informações de um usuário específico com base no ID fornecido.
+     *
+     * @param id O identificador único (ID) do usuário a ser buscado.
+     * @return Uma resposta HTTP contendo o status 200 (OK) e os dados do usuário.
+     *         Caso o usuário não seja encontrado, uma exceção será lançada.
+     */
     @GetMapping()
     public ResponseEntity<User> findUser(@PathVariable Long id) {
         var user = userService.findUser(id);
