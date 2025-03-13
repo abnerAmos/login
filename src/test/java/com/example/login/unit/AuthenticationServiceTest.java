df5ff98012661d97688c97858312c1f55bc53606
abnerAmos <abner.fsouza1@gmail.com>
2025-03-13 17:10:38 -0300

    - Testes de refreshToken

--- src/test/java/com/example/login/unit/AuthenticationServiceTest.java
+++ src/test/java/com/example/login/unit/AuthenticationServiceTest.java
@@ -15,6 +15,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
@@ -56,9 +57,8 @@ public class AuthenticationServiceTest {
     }
 
     @Test
-    @DisplayName("Deve lançar excessão ao tentar carregar usuário")
+    @DisplayName("Deve lançar UsernameNotFoundException ao tentar carregar usuário")
     public void testLoadUserByUsername_Error() {
-        User user = UserFactory.createUser(1L);
         when(userRepository.findByEmail(EMAIL)).thenReturn(null);
 
         assertThrows(UsernameNotFoundException.class, () -> authenticationService.loadUserByUsername(EMAIL));
@@ -101,5 +101,53 @@ public class AuthenticationServiceTest {
         assertEquals("existingRefreshToken", tokenResponse.refreshToken());
         verify(tokenCache).storeToken(eq(1L), eq("newAccessToken"), anyLong(), eq("accessToken"));
         verify(tokenCache, never()).storeToken(eq(1L), eq("existingRefreshToken"), anyLong(), eq("refreshToken"));
+    }
+
+    @Test
+    @DisplayName("Deve gerar novos tokens de acesso e refresh quando o refresh token é válido")
+    public void testRefreshToken_Success() {
+        String refreshTokenRequest = "validRefreshToken";
+        User user = UserFactory.createUser(1L);
+        TokenData newAccessToken = new TokenData("newAccessToken", 3600);
+        TokenData newRefreshToken = new TokenData("newRefreshToken", 7200);
+
+        when(tokenCache.isTokenInvalidated(refreshTokenRequest)).thenReturn(false);
+        when(tokenService.getSubject(refreshTokenRequest, "refreshToken")).thenReturn(user.getUsername());
+        when(userRepository.findByEmail(user.getEmail())).thenReturn(user);
+        when(tokenService.generateToken(user, "accessToken")).thenReturn(newAccessToken);
+        when(tokenService.generateToken(user, "refreshToken")).thenReturn(newRefreshToken);
+
+        TokenResponse tokenResponse = authenticationService.refreshToken(refreshTokenRequest);
+
+        assertNotNull(tokenResponse);
+        assertEquals("newAccessToken", tokenResponse.accessToken());
+        assertEquals("newRefreshToken", tokenResponse.refreshToken());
+        verify(tokenCache).storeToken(user.getId(), "newAccessToken", newAccessToken.expiration(), "accessToken");
+        verify(tokenCache).storeToken(user.getId(), "newRefreshToken", newRefreshToken.expiration(), "refreshToken");
+    }
+
+    @Test
+    @DisplayName("Deve lançar AuthenticationException quando o token é inválido ou expirado")
+    public void testRefreshToken_InvalidOrExpiredToken() {
+        String invalidRefreshToken = "invalidOrExpiredToken";
+
+        when(tokenCache.isTokenInvalidated(invalidRefreshToken)).thenReturn(true);
+
+        assertThrows(AuthenticationException.class,
+                () -> authenticationService.refreshToken(invalidRefreshToken));
+    }
+
+    @Test
+    @DisplayName("Deve lançar UsernameNotFoundException quando o usuário não é encontrado")
+    public void testRefreshToken_UserNotFound() {
+        String validRefreshToken = "validRefreshToken";
+        User user = UserFactory.createUser(1L);
+
+        when(tokenCache.isTokenInvalidated(validRefreshToken)).thenReturn(false);
+        when(tokenService.getSubject(validRefreshToken, "refreshToken")).thenReturn(user.getUsername());
+        when(userRepository.findByEmail(EMAIL)).thenReturn(null);
+
+        assertThrows(UsernameNotFoundException.class,
+                () -> authenticationService.refreshToken(validRefreshToken));
     }
 }
